043f65c585a9559cda458bdf15cbfd33042debf1
HanaHsu <kobanae@gmail.com>
2012-11-21 11:31:35 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ParagraphEditor::Application.config.secret_token = '81c35073b6ce7a169628b9307aa9a6dfa6ef9c1978aeb8174f59c79b2f4b85cb5a1020bfe1c42594c977d6713f55fa30f4b4e21c1bb2e9764cbf4cdb8e08f80a'
