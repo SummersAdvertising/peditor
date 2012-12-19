2eab6dd248aaecdbaec50d0c380e04ee94ca6797
HanaHsu <kobanae@gmail.com>
2012-12-19 16:24:53 +0800

    修正textbox在sortable div中失去作用bug

--- test/unit/article_test.rb
+++ test/unit/article_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "the truth" do
+     assert true
+   end
+
+   def test_assert
+    assert false, "this test returns false"
+   end
+
 end
