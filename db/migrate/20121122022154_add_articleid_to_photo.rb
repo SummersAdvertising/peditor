d122b05f71f023f40042c5abd7b57ca09c61f66f
Yuzhe <yuzhe.c85@gmail.com>
2013-10-15 14:43:00 +0800

    merge migration files

--- db/migrate/20121122022154_add_articleid_to_photo.rb
+++
@@ -1,5 +0,0 @@
-class AddArticleidToPhoto < ActiveRecord::Migration
-  def change
-    add_column :photos, :articleid, :int
-  end
-end
--- db/migrate/20130517104855_change_content_type.rb
+++
@@ -1,5 +0,0 @@
-class ChangeContentType < ActiveRecord::Migration
-  def change
-  	change_column :articles, :content, :text, :limit => nil
-  end
-end
